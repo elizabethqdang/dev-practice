38a471c1b7afd04201f5005718704459a79b0381
Elizabeth Dang <elizabethqdang@gmail.com>
2021-11-17 10:27:57 -0800

    server configuration

---
+++ recapp-backend/README.md
@@ -0,0 +1,17 @@
+# Question App
+
+## Functional Requirements
+	Question Form
+		Name
+		Content
+		Live
+		Repo
+	Index (Show/Get All Questions)
+	Question
+		linkes to repo & live
+		button mark as answered (Delete)
+		
+# Relational
+	PostgreSQL
+	
+	
---
+++ recapp-backend/index.js
@@ -0,0 +1,22 @@
+// import express from 'express';
+
+// function (class) () => { does a bunch of stuff, has additional function }
+const express = require('express');
+
+// express is a function imported from the 'express' module. here invoke 'express' with no arguments, setting its return equal to an arbitrary value 'app'
+const app = express();
+
+console.log(app);
+app.get('/', (req, res) => res.send("This Works"))
+
+// we then declare a port value, 5000 in this case
+const port = process.env.PORT || 5000
+
+// const process = {
+// 	env: {
+// 		PORT: 8000
+// 	}
+// }
+
+// finally let's use the 'listen' function provided by our 'app' instancde. this function, when invoked with a port and callback function, will maintain an open connection to your new server
+app.listen(port, () => console.log(`Server is up on port ${port}`))
