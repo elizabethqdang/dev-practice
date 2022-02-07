709d33c40587fb699907b2e2f42075c874c3dc9f
Elizabeth Dang <elizabethqdang@gmail.com>
2022-02-07 15:07:04 -0800

    models/question

---
+++ recapp-backend/config/keys.js
@@ -0,0 +1,6 @@
+module.exports ={
+	mongoURI: 'mongodb+srv://DevPracticeUser:nocPE8J4goW4e1V7@devpracticecluster.rc4wi.mongodb.net/test'
+}
+
+// const username = 'DevPracticeUser'
+// const pw = 'nocPE8J4goW4e1V7'
