8c5d89cec82421761d5bb9946a951fffc3011936
Elizabeth Dang <elizabethqdang@gmail.com>
2022-02-28 12:00:00 -0700

    recapp 1.0 backend

--- recapp-backend/routes/api/questions.js
+++ recapp-backend/routes/api/questions.js
@@ -1,12 +1,28 @@
-const express = require('express') // function 
+const express = require('express');
+// const { eventNames } = require('../../models/question');
 const router = express.Router();
 
 const Question = require('../../models/question')
 
-router.get('/', (req, res) => { 
-	const ids = []
+router.get('/', (req, res) => {
 	Question.find()
-		.then(questions => { 
-			questions.forEach(res.json(questions))
+		.then(questions => res.json(questions))
 			.catch(err => res.status(404).json(err))
 })
+
+// router.get('/names', (req, res) => { 
+// 	const names = []
+// 	Question.find()
+// 		.then(questions => { 
+// 			questions.forEach(question => {
+// 				names.push(question.name)
+// 			})
+// 		}).then(res.json(names))
+// 			.catch(err => res.status(404).json(err))
+// });	
+
+// const res = {
+// 	questions: {
+
+// 	}
+// }
